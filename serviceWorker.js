2e1cb90b1bbade8638d0505c3cbdb469a50ff163
makersmecca <37550248+makersmecca@users.noreply.github.com>
2021-08-02 22:05:30 +0530

    Add files via upload

---
+++ serviceWorker.js
@@ -0,0 +1,31 @@
+const staticSimpleCounter = "the-simple-counter"
+const assets = [
+    "/",
+    "index.html",
+    "style.css",
+    "index.js",
+    "images/Picture1.ico"
+]
+
+self.addEventListener("install", installEvent =>
+{
+    installEvent.waitUntil
+    (
+        caches.open(staticSimpleCounter).then(cache=>
+            {
+                cache.addAll(assets)
+            })
+    )
+})
+
+self.addEventListener("fetch", fetchEvent =>
+{
+    fetchEvent.respondWith
+    (
+        caches.match(fetchEvent.request).then(res=>
+            {
+                return res||fetch(fetchEvent.request)
+            })
+    )
+})
+
