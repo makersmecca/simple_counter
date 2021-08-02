5bc9f0b3e0582b77d78de54fd3144496952f2bee
makersmecca <37550248+makersmecca@users.noreply.github.com>
2021-08-02 22:04:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+let pressEl = document.getElementById("view")
+
+let count = 0
+pressEl.textContent = count
+
+function pressed()
+{
+    console.log("Button pressed")
+    count+=1
+    pressEl.textContent = count
+}
+
+function resets()
+{
+    console.log("Counter Reset")
+    count=0
+    pressEl.textContent = count
+}
+
+if("serviceWorker" in navigator)
+{
+    window.addEventListener("load",function()
+    {
+        navigator.serviceWorker
+        .register("/serviceWorker.js")
+        .then(res=> console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err))
+
+    })
+}
