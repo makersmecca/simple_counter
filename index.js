cd6c320672c2f749a58accf2a4bd2e9a77d20de6
makersmecca <37550248+makersmecca@users.noreply.github.com>
2021-08-02 15:28:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,3 +17,13 @@ function resets()
     pressEl.textContent = count
 }
 
+if("serviceWorker" in navigator)
+{
+    window.addEventListener("load",function()
+    {
+        navigator.serviceWorker
+        .register("/serviceWorker.js")
+        .then(res=>console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered" , err))
+    })
+}
